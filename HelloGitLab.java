0a6e8efbd05bc55f2f92f3fff0a1c421bcb53ba3
Sai lavanya <Sailavanya@wipro.com>
2025-04-16 18:18:19 +0000

    Initial commit: HelloGitLab Java program

--- HelloGitLab.java
+++ HelloGitLab.java
@@ -1,5 +1,5 @@
 public class HelloGitLab {
-     public static void main(string[] args) {
+     public static void main(String[] args) {
            System.out.println("Hello from Java to GitLab!");
      }
 }
